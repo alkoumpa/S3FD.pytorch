50cb571ea8cbf406b84773ace5471d5c3c847db4
alkoumpa <alkoumpa@uth.gr>
2020-11-19 19:59:24 -0500

    wrap in torch.no_grad at test time

--- s3fd.py
+++ s3fd.py
@@ -146,14 +146,13 @@ class S3FD(nn.Module):
         loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
 
-       
         if self.phase == 'test':
-            output = self.detect(
-                loc.view(loc.size(0), -1, 4),                   # loc preds
-                self.softmax(conf.view(conf.size(0), -1,
-                                       self.num_classes)),                # conf preds
-                self.priors.type(type(x.data))                  # default boxes
-            )
+            with torch.no_grad():
+                output = self.detect.forward(
+                    loc.view(loc.size(0), -1, 4), # loc preds
+                    self.softmax(conf.view(conf.size(0), -1, self.num_classes)), # conf preds
+                    self.priors.to(x.device)  # default boxes
+                )
 
         else:
             output = (
